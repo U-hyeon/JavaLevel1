d733684d0b49da3b5230e7cb145a24a9b48711fb
U-hyeon <jeon-uhyeon@jeon-uhyeons-MacBook-Pro.local>
2024-11-18 01:50:56 +0900

    comment주석

---
+++ src/Comment.java
@@ -0,0 +1,11 @@
+public class Comment {
+    public static void main(String[] args) {
+        System.out.println("Hello Java1"); // hello java1 출력
+        //System.out.println("Hello Java2");
+
+        /* 여러줄 주석
+        System.out.println("Hello Java3"); // hello java1 출력
+        System.out.println("Hello Java4");
+        */
+    }
+}
