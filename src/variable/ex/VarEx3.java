b0806cd6da3a3d36a397d3814ca572aa72c7cd13
U-hyeon_MAC <wisebuddyu@gmail.com>
2024-11-22 01:43:16 +0900

    java variable ex

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,13 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longNum = 10000000000L;
+        System.out.println(longNum);
+
+        boolean boolNum = true;
+        System.out.println(boolNum);
+        // soutv
+        System.out.println("boolNum = " + boolNum);
+    }
+}
